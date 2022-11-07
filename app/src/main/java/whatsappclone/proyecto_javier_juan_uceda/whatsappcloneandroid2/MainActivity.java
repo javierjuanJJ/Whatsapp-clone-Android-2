834d376bc97d185d9a97f884227f91a6f6f84a24
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-07 16:04:50 +0100

    Initial commit

---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/MainActivity.java
@@ -0,0 +1,14 @@
+package whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
