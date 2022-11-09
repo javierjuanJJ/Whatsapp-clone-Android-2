e28134c1ab74ebe8d5b9039cfe471d82a4eff5dd
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-09 17:31:08 +0100

    Commit 14: No friends layout

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/view/stanup/SplashActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/view/stanup/SplashActivity.java
@@ -6,23 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.R;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.MainActivity;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.auth.PhoneLoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, PhoneLoginActivity.class));
-                finish();
-            }
-        }, 3000);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, 3000);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, WelcomeScreenActivity.class));
+                    finish();
+                }
+            }, 3000);
+        }
 
     }
 }
