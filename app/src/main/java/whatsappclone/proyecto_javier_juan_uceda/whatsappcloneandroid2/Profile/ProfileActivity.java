82a2102873e4c4040951ffb1c086fe974c74f08c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-10 17:36:03 +0100

    Commit 20: Show Bottom sheet dialog

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/Profile/ProfileActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/Profile/ProfileActivity.java
@@ -2,14 +2,19 @@ package whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Profile;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -26,6 +31,7 @@ public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firestore;
+    private BottomSheetDialog bottomSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +49,40 @@ public class ProfileActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        bottomSheetDialog = new BottomSheetDialog(this);
+
         if (firebaseUser != null){
             getInfo();
         }
 
+        initClickAction();
+
     }
 
     private void initClickAction() {
+        binding.fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
+    }
 
+    private void showBottomSheetDialog() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick, null);
+
+        bottomSheetDialog.setContentView(view);
+
+        bottomSheetDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                bottomSheetDialog = null;
+            }
+        });
+
+
+        bottomSheetDialog.show();
     }
 
     private void getInfo() {
