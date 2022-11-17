3213ab10325c25cc0e256cfbfb48e191f74527fc
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-17 12:20:25 +0100

    Commit 38: Dialog button click

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/Dialogs/DialogViewUser.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/Dialogs/DialogViewUser.java
@@ -1,9 +1,13 @@
 package whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Dialogs;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,10 +16,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityOptionsCompat;
+
 import com.bumptech.glide.Glide;
 
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Common.Common;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Display.ViewImageActivity;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.R;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.model.ChatList;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Chat.ChatActivity;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Profile.ProfileActivity;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Profile.UserProfileActivity;
 
 public class DialogViewUser {
    private Context context;
@@ -60,7 +71,13 @@ public class DialogViewUser {
       btnChat.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+
+            context.startActivity(new Intent(context, ChatActivity.class)
+                    .putExtra("userId",chatList.getUserID())
+                    .putExtra("userName",chatList.getUserName())
+                    .putExtra("userProfile",chatList.getUrlProfile()));
+            dialog.dismiss();
          }
       });
 
@@ -68,20 +85,41 @@ public class DialogViewUser {
          @Override
          public void onClick(View v) {
             Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
          }
       });
 
       btnInfo.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+            context.startActivity(new Intent(context, UserProfileActivity.class)
+                    .putExtra("userId",chatList.getUserID())
+                    .putExtra("userName",chatList.getUserName())
+                    .putExtra("userProfile",chatList.getUrlProfile())
+            );
+            dialog.dismiss();
          }
+
       });
 
       btnVideoCall.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
             Toast.makeText(context, "Chats click", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+         }
+      });
+
+      profile.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            profile.invalidate();
+            Drawable drawable = profile.getDrawable();
+            Common.IMAGE_BITMAP = ((BitmapDrawable)drawable).getBitmap();
+            ActivityOptionsCompat image = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) context, profile, "image");
+            Intent intent = new Intent(context, ViewImageActivity.class);
+            context.startActivity(intent,image.toBundle());
          }
       });
 
