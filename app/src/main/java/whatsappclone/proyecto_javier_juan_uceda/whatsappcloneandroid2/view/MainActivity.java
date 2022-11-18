517cc6db8e76aecb343c232eec39643f0b440c70
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-18 10:50:52 +0100

    Commit 45: Add status activity

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/view/MainActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappcloneandroid2/view/MainActivity.java
@@ -1,25 +1,45 @@
 package whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view;
 
+import static whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Profile.ProfileActivity.REQUEST_CAMERA_PHOTO;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.BuildConfig;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Contacts.ContactsActivity;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Menu.CallsFragment;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Menu.CameraFragment;
@@ -27,11 +47,14 @@ import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Menu.Chats
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.Menu.StatusFragment;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.R;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.databinding.ActivityMainBinding;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Profile.ProfileActivity;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Settings.SettingsActivity;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappcloneandroid2.view.Activities.Status.StatusActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -163,32 +186,107 @@ public class MainActivity extends AppCompatActivity {
                 switch (icon){
                     case 0: binding.fabAction.hide(); break;
                     case 1:
-
                         binding.fabAction.show();
-                        binding.fabAction.setOnClickListener(v -> {
-
-                        });
-
                         break;
                     case 2:
                         binding.btnAddStatus.setVisibility(View.VISIBLE);
                         binding.fabAction.show();
-                        binding.fabAction.setOnClickListener(v -> {
-
-                        });
                         break;
                     case 3:
                         binding.fabAction.show();
-                        binding.fabAction.setOnClickListener(v -> {
-                            startActivity(new Intent(MainActivity.this, ContactsActivity.class));
-                        });
-
                         break;
                 }
                 //binding.fabAction.show();
             }
         },400);
 
+        performOnClick(icon);
+    }
+
+
+    private void performOnClick(final int index){
+        binding.fabAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (index){
+                    case 1: startActivity(new Intent(MainActivity.this, ContactsActivity.class));break;
+                    case 2:
+                        checkCameraPermissions();
+                        Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 3:
+                        Toast.makeText(MainActivity.this, "Call", Toast.LENGTH_SHORT).show();
+                        break;
+                    default: break;
+                }
+            }
+        });
+        binding.btnAddStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Add status... ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void checkCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 222);
+        }
+        else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 222);
+        }
+        else {
+            openCamera();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("listPhotoName", String.valueOf(requestCode));
+        Log.i("listPhotoName", String.valueOf(resultCode));
+
+        if (requestCode == REQUEST_CAMERA_PHOTO && resultCode == RESULT_OK && imageUri != null) {
+
+            //imageUri = data.getData();
+            //uri = data.getData();
+            Log.i("listPhotoName", String.valueOf(imageUri));
+            startActivity(new Intent(MainActivity.this, StatusActivity.class));
+            //uploadToFirebase();
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String timeStmap = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStmap;
+
+        try {
+            File file = File.createTempFile(imageFileName, ".jpg", getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+            imageUri = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", file);
+
+            //uri = Uri.fromFile(file);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            intent.putExtra("listPhotoName", file);
+            Log.i("listPhotoName", file.getAbsolutePath());
+
+            startActivityForResult(intent, REQUEST_CAMERA_PHOTO);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
 }
